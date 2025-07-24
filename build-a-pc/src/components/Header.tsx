bcac9cc00f5761e9fda385f4075d26ad31cf64c5
Kerim Kapetanović <kerimkape@gmail.com>
2025-07-24 11:52:10 +0200

    Header.tsx fix

--- build-a-pc/src/components/Header.tsx
+++ build-a-pc/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 "use client";
+import React from "react";
 import Link from "next/link";
 import {useEffect,useState} from "react";
 import {useRouter} from 'next/navigation';
